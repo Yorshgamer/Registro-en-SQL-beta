a0a9f70ff04d3cd06e7248d0212fe8dea2955ea9
Yorshgamer <146681194+Yorshgamer@users.noreply.github.com>
2023-11-14 01:20:33 -0500

    Add files via upload

---
+++ Form3.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Diseño_ClientesCRUD
+{
+    partial class Cliente_Contacto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button6 = new System.Windows.Forms.Button();
+            this.btnRetrocedes = new System.Windows.Forms.Button();
+            this.btnSaliir = new System.Windows.Forms.Button();
+            this.txtboxNumCont = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FechaActual = new System.Windows.Forms.DateTimePicker();
+            this.btnCont = new System.Windows.Forms.Button();
+            this.btnDirec = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtboxCorreo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(649, 376);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(102, 38);
+            this.button6.TabIndex = 55;
+            this.button6.Text = "SIGUIENTE";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // btnRetrocedes
+            // 
+            this.btnRetrocedes.Location = new System.Drawing.Point(335, 376);
+            this.btnRetrocedes.Name = "btnRetrocedes";
+            this.btnRetrocedes.Size = new System.Drawing.Size(132, 38);
+            this.btnRetrocedes.TabIndex = 54;
+            this.btnRetrocedes.Text = "RETROCEDER";
+            this.btnRetrocedes.UseVisualStyleBackColor = true;
+            this.btnRetrocedes.Click += new System.EventHandler(this.btnRetrocedes_Click);
+            // 
+            // btnSaliir
+            // 
+            this.btnSaliir.Location = new System.Drawing.Point(43, 376);
+            this.btnSaliir.Name = "btnSaliir";
+            this.btnSaliir.Size = new System.Drawing.Size(98, 38);
+            this.btnSaliir.TabIndex = 53;
+            this.btnSaliir.Text = "SALIR";
+            this.btnSaliir.UseVisualStyleBackColor = true;
+            this.btnSaliir.Click += new System.EventHandler(this.btnSaliir_Click);
+            // 
+            // txtboxNumCont
+            // 
+            this.txtboxNumCont.Location = new System.Drawing.Point(319, 222);
+            this.txtboxNumCont.Name = "txtboxNumCont";
+            this.txtboxNumCont.Size = new System.Drawing.Size(245, 22);
+            this.txtboxNumCont.TabIndex = 52;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(180, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 16);
+            this.label4.TabIndex = 51;
+            this.label4.Text = "Número de contacto:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(240, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(287, 25);
+            this.label2.TabIndex = 50;
+            this.label2.Text = "METODOS DE CONTACTO";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(422, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 16);
+            this.label1.TabIndex = 49;
+            this.label1.Text = "Fecha Actual:";
+            // 
+            // FechaActual
+            // 
+            this.FechaActual.Location = new System.Drawing.Point(516, 116);
+            this.FechaActual.Name = "FechaActual";
+            this.FechaActual.Size = new System.Drawing.Size(266, 22);
+            this.FechaActual.TabIndex = 48;
+            // 
+            // btnCont
+            // 
+            this.btnCont.Location = new System.Drawing.Point(544, 25);
+            this.btnCont.Name = "btnCont";
+            this.btnCont.Size = new System.Drawing.Size(244, 64);
+            this.btnCont.TabIndex = 47;
+            this.btnCont.Text = "Metodos de Contacto";
+            this.btnCont.UseVisualStyleBackColor = true;
+            // 
+            // btnDirec
+            // 
+            this.btnDirec.Location = new System.Drawing.Point(277, 25);
+            this.btnDirec.Name = "btnDirec";
+            this.btnDirec.Size = new System.Drawing.Size(244, 64);
+            this.btnDirec.TabIndex = 46;
+            this.btnDirec.Text = "Direcciones";
+            this.btnDirec.UseVisualStyleBackColor = true;
+            this.btnDirec.Click += new System.EventHandler(this.btnDirec_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(13, 25);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(244, 64);
+            this.button9.TabIndex = 45;
+            this.button9.Text = "Datos Generales";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(180, 278);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 16);
+            this.label3.TabIndex = 57;
+            this.label3.Text = "Correo electronico:";
+            // 
+            // txtboxCorreo
+            // 
+            this.txtboxCorreo.Location = new System.Drawing.Point(319, 272);
+            this.txtboxCorreo.Name = "txtboxCorreo";
+            this.txtboxCorreo.Size = new System.Drawing.Size(245, 22);
+            this.txtboxCorreo.TabIndex = 58;
+            // 
+            // Cliente_Contacto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtboxCorreo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.btnRetrocedes);
+            this.Controls.Add(this.btnSaliir);
+            this.Controls.Add(this.txtboxNumCont);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FechaActual);
+            this.Controls.Add(this.btnCont);
+            this.Controls.Add(this.btnDirec);
+            this.Controls.Add(this.button9);
+            this.Name = "Cliente_Contacto";
+            this.Text = "MAPLIDE-Metodos_de_contacto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button btnRetrocedes;
+        private System.Windows.Forms.Button btnSaliir;
+        private System.Windows.Forms.TextBox txtboxNumCont;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker FechaActual;
+        private System.Windows.Forms.Button btnCont;
+        private System.Windows.Forms.Button btnDirec;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtboxCorreo;
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+namespace Diseño_ClientesCRUD
+{
+    public partial class Cliente_Contacto : Form
+    {
+        private Cliente_direcciones Formdirecciones;
+        private Principal FormPrincipal;
+        private Conexion con = new Conexion();
+
+        public Cliente_Contacto(Cliente_direcciones Formdirecciones)
+        {
+            InitializeComponent();
+            this.Formdirecciones = Formdirecciones;
+            FormPrincipal = Formdirecciones.GetFormPrincipal();
+        }
+
+        private void btnSaliir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnRetrocedes_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Formdirecciones.Show();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            FormPrincipal.Show();
+            this.Hide();
+        }
+
+        private void btnDirec_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Formdirecciones.Show();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string Numero = txtboxNumCont.Text;
+            string Correo = txtboxCorreo.Text;
+
+            string datosContacto = $"'{Numero}'," +$"'{Correo}',";
+            string datosDirecciones = Formdirecciones.GetDatos();
+            string datosPrincipal = FormPrincipal.GetDatos();
+            string datosFechaNaci = FormPrincipal.GetDatosFecNac();
+            string datosFecha = FormPrincipal.GetDatosFec();
+            string datosTotales = $"{datosPrincipal}{datosDirecciones}{datosContacto}"+ $"'{datosFechaNaci}',"+"'Usuario1',"+$"'{datosFecha}',"+ "'11:00:00','192.146.1'";
+
+            con.InsertarDatosCliente(datosTotales);
+            MessageBox.Show(datosTotales, "Datos Totales");
+        }
+    }
+}
